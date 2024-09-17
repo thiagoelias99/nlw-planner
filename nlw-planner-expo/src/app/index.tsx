fbe16ad0783778a54a3f3244bda7db30ef16f9bc
Thiago Elias <thiagoelias99@gmail.com>
2024-09-17 10:06:16 -0300

    Main UI

--- nlw-planner-expo/src/app/index.tsx
+++ nlw-planner-expo/src/app/index.tsx
@@ -1,12 +1,42 @@
-import { Image, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { Image, ImageBackground, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import { Input } from '@/components/input'
 import { MapPinIcon, CalendarIcon, Settings2Icon, UserRoundPlusIcon, ArrowRightIcon } from 'lucide-react-native'
 import { colors } from '@/styles/colors'
 import { Button } from '@/components/button'
 
+enum StepForm {
+  TRIP_DETAILS = 1,
+  ADD_EMAIL = 2,
+}
+
+enum MODAL {
+  NONE = 0,
+  CALENDAR = 1,
+  GUESTS = 2,
+}
 
 export default function Index() {
+  // LOADING
+  const [isCreatingTrip, setIsCreatingTrip] = useState(false)
+  const [isGettingTrip, setIsGettingTrip] = useState(true)
+
+  // DATA
+  const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS)
+  // const [selectedDates, setSelectedDates] = useState({} as DatesSelected)
+  const [destination, setDestination] = useState("")
+  const [emailToInvite, setEmailToInvite] = useState("")
+  const [emailsToInvite, setEmailsToInvite] = useState<string[]>([])
+
+  // MODAL
+  const [showModal, setShowModal] = useState(MODAL.NONE)
+
+  function handleNextStepForm() {
+    setStepForm(StepForm.ADD_EMAIL)
+  }
+
+
+
   return (
     <View className='flex-1 justify-center items-center px-5'>
       <Image
@@ -15,38 +45,55 @@ export default function Index() {
         resizeMode='contain'
       />
 
+      <Image
+        source={require('@/assets/bg.png')}
+        className='absolute'
+      />
+
       <Text className='text-zinc-400 font-regular text-center text-lg mt-3'
       >Convide seus amigos e planeje sua{"\n"}próxima viagem</Text>
 
       <View className='w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800'>
         <Input>
           <MapPinIcon color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder='Para onde?' />
+          <Input.Field
+            editable={stepForm === StepForm.TRIP_DETAILS}
+            placeholder='Para onde?' />
         </Input>
 
         <Input>
           <CalendarIcon color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder='Quando?' />
+          <Input.Field
+            editable={stepForm === StepForm.TRIP_DETAILS}
+            placeholder='Quando?' />
         </Input>
 
-        <View className="border-b py-3 border-zinc-800">
-          <Button
-            variant="secondary"
-          >
-            <Button.Title>Alterar local/data</Button.Title>
-            <Settings2Icon color={colors.lime[950]} size={20} />
-          </Button>
-        </View>
+        {stepForm === StepForm.ADD_EMAIL && (
+          <>
+            <View className="border-b py-3 border-zinc-800">
+              <Button
+                variant="secondary"
+                onPress={() => setStepForm(StepForm.TRIP_DETAILS)}
+              >
+                <Button.Title>Alterar local/data</Button.Title>
+                <Settings2Icon color={colors.lime[950]} size={20} />
+              </Button>
+            </View>
 
-        <Input>
-          <UserRoundPlusIcon color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder='Quem estará na viagem?' />
-        </Input>
+            <Input>
+              <UserRoundPlusIcon color={colors.zinc[400]} size={20} />
+              <Input.Field placeholder='Quem estará na viagem?' />
+            </Input>
+          </>
+        )}
 
         <Button
           variant="primary"
+          onPress={handleNextStepForm}
         >
-          <Button.Title>Continuar</Button.Title>
+          <Button.Title>
+            {stepForm === StepForm.TRIP_DETAILS ? "Continuar" : "Confirmar viagem"}
+          </Button.Title>
           <ArrowRightIcon color={colors.lime[950]} size={20} />
         </Button>
       </View>
